564978198a195f15bc065444428cb73a85fb42ca
AbdulrahmanYh <abdulrahman.yh@outlook.sa>
2024-09-05 00:11:45 +0300

    Update String1.java

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -1,48 +1,60 @@
 package com.example.project;
 
 
+
 public class String1 {
 	private char[] str;
 	private int size;
 
-	public String1 () {
-	   size = -1;
-	   str = new char[80];
+	public String1() {
+		size = -1;
+		str = new char[80];
 	}
-	public void append(char c) {
-        str[++size] = c;
-    }
-    public char remove() {
-        return str[size--];
-    }
-    public char getChar(int i) {
-        return str[i];
-    }
-    public int length() {
-        return size+1;
-    }
-    public void makeEmpty()
-    {
-        size = -1;
-    }
-    public void concat(String1 s)
-    {
-        for(int i = 0; i < s.length(); i++)
-            append(s.getChar(i));
-    }
-    public boolean equal(String1 s) {
-       if (s.size == this.size) {
-			for (int i = 0; i <=s.size; i++) {
-				if (s.getChar(i) != this.getChar(i))
-					return false;
-			}
-			return true;
 
+	public void append(char c) {
+		str[++size] = c;
+	}
+
+	public char remove() {
+		return str[size--];
+	}
+
+	public char getChar(int i) {
+		return str[i];
+	}
+
+	public int length() {
+		return size + 1;
+	}
+
+	public void makeEmpty() {
+		size = -1;
+	}
+
+	public void concat(String1 s) {
+		for (int i = 0; i < s.length(); i++)
+			append(s.getChar(i));
+	}
+
+	public boolean equal(String1 s) {
+		if (s.size < 0 || this.size < 0) {
+			return false;
 		}
+			if (s.size == this.size) {
+				for (int i = 0; i <= s.size; i++) {
+					if (s.getChar(i) != this.getChar(i))
+						return false;
+				}
+				return true;
+
+			}
 		return false;
 	}
-    public void reverse() {
-       for (int i = 0; i <= size / 2; i++) {
+
+	public void reverse() {
+		if(size<0)
+			return;
+		for (int i = 0; i <= size / 2; i++) {
 			char temp = str[size - i];
 			str[size - i] = str[i];
 			str[i] = temp;
