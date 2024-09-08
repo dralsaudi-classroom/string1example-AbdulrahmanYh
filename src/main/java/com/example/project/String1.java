3866ed1bd69423c19cedc74146ba4583aefedbad
AbdulrahmanYh <abdulrahman.yh@outlook.sa>
2024-09-08 19:52:16 +0300

    Update String1.java

--- src/main/java/com/example/project/String1.java
+++ src/main/java/com/example/project/String1.java
@@ -34,29 +34,22 @@ public class String1 {
 			append(s.getChar(i));
 	}
 
-	public boolean equals(String1 s) {
-		if (s.size == this.size) { // Assuming `size` is a valid field in your String1 class
-			for (int i = 0; i < s.size; i++) {
-				if (s.getChar(i) != this.getChar(i)) {
-					return false; // Return false if any character does not match
-				}
+	public boolean equal(String1 s) {
+
+		for (int i = 0; i < s.length(); i++) {
+			if (s.getChar(i) != str[i]) {
+				return false;
 			}
-			return true; // Return true if all characters match
+
+			return true; }
 		}
-		return false; // Return false if not an instance of String1 or sizes do not match
-	}
 
 	public void reverse() {
-	    if (size < 0) {
-	        return; // Return early if the string is empty
-	    }
-	    // Iterate only up to the middle of the string
-	    for (int i = 0; i <= size / 2; i++) {
-	        char temp = str[i];       // Store the character at the current index
-	        str[i] = str[size - i];   // Swap character from the end to the current index
-	        str[size - i] = temp;     // Place the stored character to the end position
-	    }
+		for (int i = 0; i <= length() / 2; i++) {
+			char temp = str[i]; 
+			str[i] = str[size - i]; 
+			str[size - i] = temp; 
+		}
 	}
-
 
 }
